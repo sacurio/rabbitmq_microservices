35258825401eb8d1364e8794283180c163bfaed5
sacurio <sandy.acurio@gmail.com>
2023-08-13 14:59:33 -0500

    Make `RabbitMq` connection

---
+++ first.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+	fmt.Println("hello world")
+}
